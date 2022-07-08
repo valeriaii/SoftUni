8a0fff27bc791279bc83e90a057f555871b55f7f
valeriaii <val.vboice@gmail.com>
2022-07-08 13:36:51 +0300

    Table Relations Exercise

---
+++ Entity Framework Core/03. Entity Relations/P03_FootballBetting/P03_FootballBetting.Data.Models/PlayerStatistic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace P03_FootballBetting.Data.Models
+{
+    //mapping class
+   public class PlayerStatistic
+    {
+        //TODO: Composite PK in Fluent API
+        [ForeignKey(nameof(Game))]
+        public int GameId { get; set; }
+        public Game Game { get; set; }
+
+        [ForeignKey(nameof(Player))]
+        public int PlayerId { get; set; }
+        public Player Player { get; set; }
+
+        public byte ScoredGoals { get; set; }
+
+        public byte Assists { get; set; }
+        public byte MinutesPlayed { get; set; }
+    }
+}
---
+++ Entity Framework Core/03. Entity Relations/P03_FootballBetting/P03_FootballBetting.Data/Config.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_FootballBetting.Data
+{
+   public  class Config
+    {
+        public const string ConnectionString = @"Server=.;Database=Bet128;Integrated Security=True;";
+    }
+}
